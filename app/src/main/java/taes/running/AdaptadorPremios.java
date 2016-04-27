d3ca724a4034c762d1f9783dffd9651c1b4706ff
Jorge <jorge.azorin@gmail.com>
2016-04-28 00:07:37 +0200

    tonteria de premios

---
+++ app/src/main/java/taes/running/AdaptadorPremios.java
@@ -0,0 +1,88 @@
+package taes.running;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by jorge on 27/04/2016.
+ */
+public class AdaptadorPremios extends PagerAdapter {
+    private Context mContext;
+
+    public AdaptadorPremios(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup collection, int position) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.premio, collection, false);
+        ImageView imagen =(ImageView) layout.findViewById(R.id.PremioImagen);
+        TextView texto =(TextView) layout.findViewById(R.id.premioTextView);
+        switch (position) {
+            case 0:
+                imagen.setImageResource(R.drawable.animals7);
+                imagen.setBackgroundColor(Color.argb(255,135, 63, 156));
+                texto.setText("Tortuga");
+                break;
+
+            case 1:
+                imagen.setImageResource(R.drawable.animals3);
+                imagen.setBackgroundColor(Color.argb(255,63, 135, 156));
+                texto.setText("Perezoso");
+                break;
+            case 2:
+                imagen.setImageResource(R.drawable.animals14);
+                imagen.setBackgroundColor(Color.argb(255,63, 156, 135));
+                texto.setText("Ñu");
+                break;
+            case 3:
+                imagen.setImageResource(R.drawable.animals5);
+                imagen.setBackgroundColor(Color.argb(255, 135, 156, 63));
+                texto.setText("Ganador de batalla");
+                break;
+            case 4:
+                imagen.setImageResource(R.drawable.animals27);
+                imagen.setBackgroundColor(Color.argb(255, 156, 135, 63));
+
+                texto.setText("Mejor maratón");
+                break;
+            case 5:
+                imagen.setImageResource(R.drawable.animals7);
+                imagen.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                texto.setText("Perezoso");
+                break;
+            default:
+                break;
+
+        }
+        collection.addView(layout);
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup collection, int position, Object view) {
+        collection.removeView((View) view);
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+       return "na";
+    }
+}
--- app/src/main/java/taes/running/FragmentPremios.java
+++ app/src/main/java/taes/running/FragmentPremios.java
@@ -2,13 +2,18 @@ package taes.running;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+
 import java.util.ArrayList;
+
+import me.relex.circleindicator.CircleIndicator;
 
 /**
  * Created by jorge on 22/04/2016.
@@ -32,6 +37,18 @@ public class FragmentPremios extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
 
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.premios, container, false);
+
+
+        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+        viewPager.setClipToPadding(false);
+        viewPager.setPadding(100,100,100,100);
+        viewPager.setPageMargin(50);
+
+        viewPager.setAdapter(new AdaptadorPremios(getContext()));
+        CircleIndicator indicator = (CircleIndicator) rootView.findViewById(R.id.indicator);
+        viewPager.setCurrentItem(3);
+        indicator.setViewPager(viewPager);
+
         return rootView;
     }
 }
